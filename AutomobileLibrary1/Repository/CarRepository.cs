755b2b63ecd5d2e2de4082626b20eae288d28f5c
Minh <minhndhe171146@fpt.edu.vn>
2024-03-13 15:40:21 +0700

    Add library

---
+++ AutomobileLibrary1/Repository/CarRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobileLibrary1.DataAccess;
+
+namespace AutomobileLibrary1.Repository
+{
+    public class CarRepository : ICarRepository
+    {
+        public Car GetCarByID(int carId) => CarDAO.Instance.GetCarByID(carId);
+        public IEnumerable<Car> GetCars() => CarDAO.Instance.GetCarList();
+        public void InsertCar(Car car) => CarDAO.Instance.AddNew(car);
+        public void DeleteCar(int carId) => CarDAO.Instance.Remove(carId);
+        public void UpdateCar(Car car) => CarDAO.Instance.Update(car);
+    }
+}
